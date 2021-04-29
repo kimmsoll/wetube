81d05e078e2bcb7f55845d53b74e84a87b993714
kim <kimmsoll@naver.com>
2021-04-29 14:56:15 +0900

    wetube

--- app.js
+++ app.js
@@ -5,16 +5,32 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
+
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan("dev"));
-app.use(helmet());
 
-app.use("/", globalRouter);
-app.use("/users", userRouter);
-app.use("/videos", videoRouter);
+app.use(localsMiddleware);
+
+app.use(function(req,res,next){
+    res.setHeader(
+        "Content-Security-Policy",
+        "script-src 'self'https://archive.org"
+    );
+    return next();
+});
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,23 @@
 import express from "express";
+import routes from "../routes";
+import { deleteVideo, getEditVideo, getUpload, postEditVideo, postUpload, videoDetail } from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
+
+// Upload
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+// Video Detail
+videoRouter.get(routes.videoDetail(), videoDetail);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
+videoRouter.get(routes.deleteVideo(), deleteVideo);
+
 
 export default videoRouter;
